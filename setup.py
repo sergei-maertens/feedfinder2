afd967caae773fe431a121c052564314d4db6db6
Sergei Maertens <sergei@maykinmedia.nl>
2016-07-18 14:32:37 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="aio-feedfinder2",
-    version='0.2.0',
+    version='0.2.1',
     url="https://github.com/sergei-maertens/feedfinder2",
     license="MIT",
     author="Sergei Maertens, Dan Foreman-Mackey",
